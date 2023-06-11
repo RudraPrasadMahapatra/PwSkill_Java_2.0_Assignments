38818b5ba18292b87ee46febf4008e4581c165cf
RUDRA PRASAD MAHAPATRA <115487403+RudraPrasadMahapatra@users.noreply.github.com>
2023-06-11 12:22:06 +0530

    Add files via upload

---
+++ Assignment_8_String/Q4/StringLength2.java
@@ -0,0 +1,14 @@
+//3. How do you find the length of a string in java explain with an example ?
+/*
+    We can find the length of a string using the length() method.
+ */
+//Eg:
+        public class StringLength2 {
+            public static void main(String[] args) {
+
+                    String str = "Rudra Prasad";
+                    int length = str.length();
+                    System.out.println(length);
+                }
+
+        }
