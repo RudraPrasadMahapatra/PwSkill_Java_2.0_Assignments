0bd51524a440bf5472649d4fd5522bc54e9b64c6
RUDRA PRASAD MAHAPATRA <115487403+RudraPrasadMahapatra@users.noreply.github.com>
2023-06-11 11:46:24 +0530

    Add files via upload

---
+++ Assignment_8_String/Q4/StringLength.java
@@ -0,0 +1,8 @@
+//5. Write a program to find the length of the string "refrigerator".
+        public class StringLength {
+            public static void main(String[] args) {
+            String str = "refrigerator";
+            int length = str.length();
+                System.out.println(length);
+            }
+        }
