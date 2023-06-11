9af039457f764d474ee97c68bef83bea5b418904
RUDRA PRASAD MAHAPATRA <115487403+RudraPrasadMahapatra@users.noreply.github.com>
2023-06-11 09:20:43 +0530

    Update StringInput.java

--- Assignment_8_String/Q4/StringInput.java
+++ Assignment_8_String/Q4/StringInput.java
@@ -1,1 +1,13 @@
+//1. Write a simple string program to take input from user.
+        import java.util.Scanner;
+        public class StringInput {
+            public static void main(String[] args) {
+                Scanner sc = new Scanner(System.in);
+                String s1 = new String();
+                System.out.println("Enter the String :");
+                s1 = sc.nextLine();
+                System.out.println(s1);
+
+            }
+}
 
